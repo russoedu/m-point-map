779fa806da933161aacee1085dea4dadaa2a6a6e
russoedu <russoedu@gmail.com>
2016-07-18 15:23:03 -0300

    [master] Using 'm' instead of 'u' for all Moblets classes

--- moblet/m-point-map-institutions.js
+++ moblet/m-point-map-institutions.js
@@ -292,6 +292,7 @@ module.exports = {
           };
           callback(userLocation);
         }, function() {
+          browserSupportFlag = false;
           callback(defaultLocation);
         });
       } else {
